36afdbd8a80cfb4fcdf3a7a05dd869f93eaf35b7
António Rocha <afgrocha98@gmail.com>
2023-06-05 16:39:41 +0100

    limit room size to 2

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const server = http.createServer(app);
 const { v4: uuidV4 } = require('uuid')
 const { Server } = require('socket.io');
 
-
 app.set('view engine','ejs')
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
@@ -34,9 +33,15 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
   console.log(`User connected ${socket.id}`);
   socket.on('join-room', (id, userId) => {
-    userIds.push(socket.id)
-    socket.join("room-"+id);
-    io.sockets.in("room-"+id).emit('connected', id, socket.id);
+    if(userIds.length < 2) {
+      userIds.push(socket.id)
+      socket.join("room-"+id);
+      io.sockets.in("room-"+id).emit('connected', id, socket.id);
+    } 
+    else {
+      io.to(socket.id).emit('full-room');
+    }
+      
   })
   socket.on('disconnect', () => {
     console.log(`A user has disconnected ${socket.id}`);
@@ -65,6 +70,10 @@ io.on('connection', (socket) => {
 
   socket.on('queen-ex-move', (roomId, player, kingPosition, queenPosition) => {
     io.sockets.in("room-"+roomId).emit('queen-ex-move-from-server', player, kingPosition, queenPosition)
+  })
+
+  socket.on('king-state', (roomId, player, checkmate) => {
+    io.sockets.in("room-"+roomId).emit('king-state-animation', player, checkmate)
   })
 });
 
