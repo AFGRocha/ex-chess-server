37e177f29c07011622912c0c1f8c683413adca03
António Rocha <afgrocha98@gmail.com>
2023-11-19 19:27:06 +0000

    local host

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { Server } = require('socket.io');
 
 app.set('view engine','ejs')
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/EX-Chess/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
